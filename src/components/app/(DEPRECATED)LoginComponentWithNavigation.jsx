f1468be3d8ffea8aff9d07212ca5e45ec0582e45
Allen <44152004+allenfc0@users.noreply.github.com>
2022-05-08 01:12:05 -0700

    Delete src directory

--- src/components/app/(DEPRECATED)LoginComponentWithNavigation.jsx
+++
@@ -1,7 +0,0 @@
-// import {Route} from 'react-router-dom'
-// import LoginComponent from './LoginComponent';
-// import withNavigation from './WithNavigation';
-
-// const LoginComponentWithNavigation = withNavigation(LoginComponent);
-
-// <Route path="/login" element={<LoginComponentWithNavigation />} />
